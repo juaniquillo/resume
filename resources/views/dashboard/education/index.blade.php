c2dae572e335d06dc636759ee4ac68ed5a8caefb
Juaniquillo <juaniquillo@gmail.com>
2026-04-10 16:44:36 -0400

    refactor: small view changes

--- resources/views/dashboard/education/index.blade.php
+++ resources/views/dashboard/education/index.blade.php
@@ -2,7 +2,7 @@
     
     <flux:heading size="xl" level="1">{{ __("Education") }}</flux:heading>
     
-    <div class="max-w-xl mt-6 flex h-full w-full flex-1 flex-col gap-4 rounded-xl">
+    <div class="max-w-xl">
         {{ $form }}
     </div>
 </x-layouts::app>
