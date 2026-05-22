c9b5706921dd9e3e011824b495efa04a2456d9c9
Juaniquillo <juaniquillo@gmail.com>
2026-05-22 17:40:56 -0400

    refactor: make section visibility form less wide

--- resources/views/dashboard/options/visibility.blade.php
+++ resources/views/dashboard/options/visibility.blade.php
@@ -2,7 +2,7 @@
     
     <flux:heading size="xl" level="1">{{ __("Section Visibility") }}</flux:heading>
     
-    <div class="max-w-xl mt-6">
+    <div class="max-w-md mt-6">
         {{ $form }}
     </div>
 </x-layouts::app>
