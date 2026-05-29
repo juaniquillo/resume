c6029dad7ff13680bec4d97e3eca636010711e5a
Juaniquillo <juaniquillo@gmail.com>
2026-05-29 12:13:22 -0400

    fix: use redirect for the section visibility update

--- resources/views/livewire/options/section-visibility.blade.php
+++ resources/views/livewire/options/section-visibility.blade.php
@@ -1,5 +1,4 @@
 <div>
-    <x-alerts class="mb-4 text-sm" />
     <flux:heading size="xl" level="1">{{ __("Section Visibility") }}</flux:heading>
     <div class="max-w-md mt-6">
         {{ $form }}
