f8070bd54a9a03f1a6211b1bf4c5e2db45422095
juaniquillo <93321+juaniquillo@users.noreply.github.com>
2026-06-03 23:47:32 +0000

    style: fix styling [skip ci]

--- app/Http/Controllers/ResumeOgController.php
+++ app/Http/Controllers/ResumeOgController.php
@@ -1,8 +1,11 @@
 <?php
+
 namespace App\Http\Controllers;
+
 use App\Models\User;
 use Illuminate\Support\Facades\Storage;
 use Spatie\LaravelScreenshot\Facades\Screenshot;
+
 class ResumeOgController extends Controller
 {
     public function show(User $user)
@@ -12,11 +15,12 @@ class ResumeOgController extends Controller
             'basics' => $user->resumeBasics(),
         ]);
     }
+
     public function image(User $user)
     {
         $path = "og-images/ogg-{$user->id}.png";
-        
-        if (!Storage::exists($path)) {
+
+        if (! Storage::exists($path)) {
             Screenshot::url(route('resume.og.show', $user))
                 ->width(1200)
                 ->height(630)
