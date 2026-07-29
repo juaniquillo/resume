e664c736fe1748182816cb5b13a9302c4f4565d1
Juaniquillo <juaniquillo@gmail.com>
2026-07-29 11:51:14 -0400

    fix: change input type to url

--- app/Cruds/Schema/Works/Inputs/UrlFactory.php
+++ app/Cruds/Schema/Works/Inputs/UrlFactory.php
@@ -61,6 +61,7 @@ class UrlFactory
                     (new DefaultAttributeBag)
                         ->setInputAttributes([
                             'label' => self::LABEL,
+                            'type' => 'url',
                         ])
                 )
         );
