b6368d78aac2971e36273c85cb057541b2f3ae07
Juaniquillo <juaniquillo@gmail.com>
2026-08-01 22:37:43 -0400

    fix: fix highlight sections

--- app/Http/Controllers/ProjectHighlightsController.php
+++ app/Http/Controllers/ProjectHighlightsController.php
@@ -2,9 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use App\Actions\Resume\Common\UpdateHighlight;
-use App\Cruds\Schema\Highlights\HighlightsCrud;
-use App\Http\Requests\HighlightsFormRequest;
 use App\Models\Project;
 use Illuminate\Http\Request;
 
@@ -15,88 +12,7 @@ class ProjectHighlightsController extends Controller
         /** @var Project $project */
         $project = $request->user()->projects()->findOrFail($id);
 
-        $crud = HighlightsCrud::build(
-            values: $request->old(),
-            errors: $request->session()->get('errors')?->toArray() ?? [],
-            baseRoute: 'dashboard.projects.highlights',
-        );
-
-        $crud->setFormAction(
-            route('dashboard.projects.highlights.store', $id)
-        );
-
-        $table = null;
-        $highlights = $project->highlights()->paginate(10);
-
-        if (! $highlights->isEmpty()) {
-            $table = $crud->makeTable($highlights);
-        }
-
         return view('dashboard.projects.highlights.index')
-            ->with('form', $crud->formWithTextareaSpanFull())
-            ->with('table', $table)
-            ->with('paginator', $highlights);
-    }
-
-    public function store(HighlightsFormRequest $request, int $id)
-    {
-        $validated = $request->validated();
-
-        /** @var Project $project */
-        $project = $request->user()->projects()->findOrFail($id);
-        $project->highlights()->create($validated);
-
-        return back()
-            ->with('success', 'Highlight created successfully.');
-    }
-
-    public function edit(Request $request, int $id, int $highlightId)
-    {
-        /** @var Project $project */
-        $project = $request->user()->projects()->findOrFail($id);
-        $highlight = $project->highlights()->findOrFail($highlightId);
-        $values = $request->old();
-        $errors = $request->session()->get('errors')?->toArray() ?? [];
-
-        $crud = HighlightsCrud::build(
-            values: $values,
-            errors: $errors,
-            model: $highlight,
-            baseRoute: 'dashboard.projects.highlights',
-        );
-
-        $crud->setFormAction(
-            route('dashboard.projects.highlights.update', [$id, $highlightId])
-        );
-
-        $form = $crud->formWithTextareaSpanFull();
-
-        return view('dashboard.projects.highlights.edit')
-            ->with('form', $form)
-            ->with('highlight', $highlight);
-    }
-
-    public function update(HighlightsFormRequest $request, int $id, int $highlightId)
-    {
-        /** @var Project $project */
-        $project = $request->user()->projects()->findOrFail($id);
-        $highlight = $project->highlights()->findOrFail($highlightId);
-
-        (new UpdateHighlight($request->validated(), $highlight))->handle();
-
-        return back()
-            ->with('success', 'Highlight updated successfully.');
-    }
-
-    public function destroy(Request $request, int $id, int $highlightId)
-    {
-        /** @var Project $project */
-        $project = $request->user()->projects()->findOrFail($id);
-        $highlight = $project->highlights()->findOrFail($highlightId);
-
-        $highlight->delete();
-
-        return back()
-            ->with('success', 'Highlight deleted successfully.');
+            ->with('model', $project);
     }
 }
--- resources/views/dashboard/projects/highlights/index.blade.php
+++ resources/views/dashboard/projects/highlights/index.blade.php
@@ -1,23 +1,16 @@
 <x-layouts::app :title="__('Dashboard')">
 
     <flux:heading size="xl" level="1">{{ __("Project Highlights") }}</flux:heading>
-    
+
     <div class="mt-6">
-        <flux:button variant="primary" size="xs" :href="route('dashboard.projects')">Go back to Projects</flux:button>
+        <flux:button variant="primary" size="xs" :href="route('dashboard.projects')" wire:navigate >Go back to Projects</flux:button>
     </div>
-
+    
     <div class="max-w-xl mt-6">
-        {{ $form }}
+        <livewire:resume.highlights.create-highlight :model="$model" />
     </div>
 
-    @if ($table ?? null)
-        <flux:separator variant="subtle" class="mt-6" />
-        <div class="px-5 py-2 bg-gray-100 dark:bg-back-table border border-gray-300 dark:border-slate-700 rounded-lg mt-6 shadow">
-            {{ $table }}
+    <livewire:resume.highlights.highlight-table :model="$model" />
+    
 
-            <div class="py-2">
-                {{ $paginator->links() }}
-            </div>
-        </div>
-    @endif
 </x-layouts::app>
