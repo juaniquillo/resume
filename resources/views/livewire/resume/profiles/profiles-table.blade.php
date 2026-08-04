6345705202232422fbe972dba9044aa6fa08376e
Juaniquillo <juaniquillo@gmail.com>
2026-08-04 11:05:27 -0400

    refactor: add table container component where missing

--- resources/views/livewire/resume/profiles/profiles-table.blade.php
+++ resources/views/livewire/resume/profiles/profiles-table.blade.php
@@ -1,3 +1,11 @@
 <div>
-    {{ $table }}
+    @if ($table ?? null)
+
+        <flux:separator variant="subtle" class="mt-6" />
+        
+        <x-table-container>
+            {{ $table }}
+        </x-table-container>
+
+    @endif
 </div>
