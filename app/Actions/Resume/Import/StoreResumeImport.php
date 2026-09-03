b8378754264dc9d0ee1cd1605ee8a828e6c36112
Juaniquillo <juaniquillo@gmail.com>
2026-09-03 16:59:30 -0400

    fix: receive the name value in StoreResumeImport

--- app/Actions/Resume/Import/StoreResumeImport.php
+++ app/Actions/Resume/Import/StoreResumeImport.php
@@ -14,7 +14,7 @@ use JustSteveKing\Resume\Factories\ResumeFactory;
 class StoreResumeImport
 {
     /**
-     * @param  array{resume_file: UploadedFile}  $data
+     * @param  array{name: string, resume_file: UploadedFile}  $data
      */
     public function handle(User $user, array $data): ResumeImport
     {
@@ -33,11 +33,10 @@ class StoreResumeImport
         $path = $file->store('imports/resumes');
 
         $nameField = NameFactory::NAME;
-        $nameValue = $data[$nameField] ?? null;
 
         /** @var ResumeImport $import */
         $import = $user->resumeImports()->create([
-            $nameField => $nameValue,
+            $nameField => $data[$nameField],
             'file_path' => $path,
             'file_name' => $file->getClientOriginalName(),
             'status' => ProcessStatus::PENDING,
