e763d6712ab39b4a388cd78a8b167caf6984faf4
Juaniquillo <juaniquillo@gmail.com>
2026-09-03 12:11:49 -0400

    refactor: minor html and blade reactor and fix

--- resources/views/components/table-container.blade.php
+++ resources/views/components/table-container.blade.php
@@ -2,7 +2,11 @@
     'paginator' => null
 ])
 
-<div class="px-5 py-2 bg-gray-100 dark:bg-back-table border border-gray-300 dark:border-slate-700 rounded-lg mt-6 shadow">
+@php
+    $classes = 'px-5 py-2 bg-gray-100 dark:bg-back-table border border-gray-300 dark:border-slate-700 rounded-lg mt-6 shadow';
+@endphp
+
+<div {{ $attributes->merge(['class' => $classes]) }}>
     
     {{ $slot }}
 
